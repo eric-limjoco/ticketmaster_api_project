d7811b57513011a3a6d5af2e86bbdcdd5355fc3a
Eric Limjoco <ericlimjoco@gmail.com>
2022-03-23 19:24:57 -0400

    Add ability to get top 20 events in the US

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const button = document.querySelector('button')
-const userInput = document.querySelector('input')
-const resultsSection = document.querySelector('resultsSection')
+const startDatePicker = document.querySelector('input#date-start')
+const endDatePicker = document.querySelector('input#date-end')
+const resultsSection = document.querySelector('.results')
+const resultsList = document.querySelector('.results-list')
+
+const populateEvents = (events) => {
+  events.forEach(e => {
+    let eventItem = document.createElement('li')
+    eventItem.innerText = `${e.name} - ${e.dates.start.localDate}`
+    resultsList.appendChild(eventItem)
+  })
+}
 
 
 button.addEventListener('click', async () => {
+  console.log(startDatePicker.value)
+  console.log(endDatePicker.value)
   let url = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=Z1cG4yVwExfaCPlK8UVUreeoVcZTNaSg'
   let response = await axios.get(url)
   console.log(response)
   let events = response.data['_embedded'].events
-  // let name = response.data['_embedded'].events[0].name
-  div.innerText = name
+  populateEvents(events)
 })
