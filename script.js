a6c5c748f4b127b3898e634371fce384786273f0
Eric Limjoco <ericlimjoco@gmail.com>
2022-03-23 19:42:06 -0400

    Add date filter

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=Z1cG4yVwExfaCPlK8UVUreeoVcZTNaSg'
 const button = document.querySelector('button')
 const startDatePicker = document.querySelector('input#date-start')
 const endDatePicker = document.querySelector('input#date-end')
@@ -5,6 +6,7 @@ const resultsSection = document.querySelector('.results')
 const resultsList = document.querySelector('.results-list')
 
 const populateEvents = (events) => {
+  resultsList.innerHTML = ''
   events.forEach(e => {
     let eventItem = document.createElement('li')
     eventItem.innerText = `${e.name} - ${e.dates.start.localDate}`
@@ -14,9 +16,9 @@ const populateEvents = (events) => {
 
 
 button.addEventListener('click', async () => {
-  console.log(startDatePicker.value)
-  console.log(endDatePicker.value)
-  let url = 'https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=Z1cG4yVwExfaCPlK8UVUreeoVcZTNaSg'
+  let startDate = startDatePicker.value
+  let endDate = endDatePicker.value
+  let url = `${baseUrl}&countryCode=US&startDateTime=${startDate}T19:02:00Z&endDateTime=${endDate}T19:02:00Z`
   let response = await axios.get(url)
   console.log(response)
   let events = response.data['_embedded'].events
