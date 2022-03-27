e85136a72361ad3df705f2d5b4b21bbf59cf4295
Eric Limjoco <ericlimjoco@gmail.com>
2022-03-27 18:34:11 -0400

    Clean up code and add clear search button

--- script.js
+++ script.js
@@ -1,19 +1,58 @@
 const baseUrl = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=Z1cG4yVwExfaCPlK8UVUreeoVcZTNaSg'
 const apiKey = 'Z1cG4yVwExfaCPlK8UVUreeoVcZTNaSg'
-const button = document.querySelector('button#search')
+
 const zipCodeInput = document.querySelector('input#zip-code')
 const startDatePicker = document.querySelector('input#date-start')
 const endDatePicker = document.querySelector('input#date-end')
-const resultsSection = document.querySelector('.results')
-const resultsList = document.querySelector('.results-list')
+
+const searchButton = document.querySelector('button#search')
+const clearButton = document.querySelector('button#clear')
 const loadMoreButton = document.querySelector('button#load-more')
+
+const resultsList = document.querySelector('.results-list')
+
 let nextLink = ''
 
-const clearEvents = () => {
-  resultsList.innerHTML = ''
-  window.localStorage.removeItem('events')
+// Use Zippopotam API to convert zip -> lat/long for Ticketmaster API
+const getLocationFromZip = async (zipCode) => {
+  const url = `https://api.zippopotam.us/us/${zipCode}`
+  const res = await axios.get(url)
+  const lat = res.data.places[0].latitude
+  const long = res.data.places[0].longitude
+  return { lat, long }
 }
 
+// Add Events to the DOM
+const populateEvents = (events) => {
+  events.forEach(e => {
+    const eventItem = document.createElement('li')
+    const eventTitle = document.createElement('h3')
+    const eventLocation = document.createElement('div')
+    const eventDate = document.createElement('div')
+    const eventLink = document.createElement('a')
+
+    eventTitle.innerText = e.name
+    eventLocation.innerText = `${e._embedded.venues[0].city.name}, ${e._embedded.venues[0].state.stateCode}`
+    eventDate.innerText = (new Date(e.dates.start.dateTime)).toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' })
+    eventLink.innerText = 'More information'
+    eventLink.href = './details.html'
+    eventLink.addEventListener('click', () => { window.localStorage.setItem('currentEvent', e.id) })
+
+    eventItem.appendChild(eventTitle)
+    eventItem.appendChild(eventLocation)
+    eventItem.appendChild(eventDate)
+    eventItem.appendChild(eventLink)
+    resultsList.appendChild(eventItem)
+  })
+}
+
+// Load existing Events into the DOM
+const loadEvents = () => {
+  const existingEvents = JSON.parse(window.localStorage.getItem('events') || "[]")
+  if (existingEvents) populateEvents(existingEvents)
+}
+
+// Store Events on window.localStorage
 const storeEvents = (events) => {
   let existingEvents = JSON.parse(window.localStorage.getItem('events'))
   if (!existingEvents) {
@@ -24,49 +63,24 @@ const storeEvents = (events) => {
   }
 }
 
-const populateEvents = (events) => {
-  events.forEach(e => {
-    const eventItem = document.createElement('li')
-    const eventTitle = document.createElement('h3')
-    eventTitle.innerText = e.name
-    const eventLocation = document.createElement('div')
-    eventLocation.innerText = `${e._embedded.venues[0].city.name}, ${e._embedded.venues[0].state.stateCode}`
-    const eventDate = document.createElement('div')
-    const d = new Date(e.dates.start.dateTime)
-    eventDate.innerText = d.toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' })
-    const detailLink = document.createElement('a')
-    detailLink.innerText = 'More information'
-    detailLink.href = './details.html'
-    detailLink.addEventListener('click', () => {
-      window.localStorage.setItem('currentEvent', e.id)
-    })
-
-    eventItem.appendChild(eventTitle)
-    eventItem.appendChild(eventLocation)
-    eventItem.appendChild(eventDate)
-    eventItem.appendChild(detailLink)
-
-    resultsList.appendChild(eventItem)
-  })
+// Remove Events from DOM and window.localStorage
+const clearEvents = () => {
+  resultsList.innerHTML = ''
+  window.localStorage.removeItem('events')
 }
 
-const getLocation = async (zipCode) => {
-  const url = `https://api.zippopotam.us/us/${zipCode}`
-  const res = await axios.get(url)
-  const lat = res.data.places[0].latitude
-  const long = res.data.places[0].longitude
-  return { lat, long }
-}
-
-
-button.addEventListener('click', async () => {
+// New search event
+searchButton.addEventListener('click', async () => {
   const zipCode = zipCodeInput.value
-  const location = await getLocation(zipCode);
+  const location = await getLocationFromZip(zipCode);
   const startDate = startDatePicker.value
   const endDate = endDatePicker.value
+
   const url = `${baseUrl}&localStartEndDateTime=${startDate}T00:00:00,${endDate}T23:59:59&sort=date,asc&radius=50&unit=miles&latlong=${location.lat},${location.long}&source=ticketmaster`
+
   const response = await axios.get(url)
   const events = response.data._embedded.events
+
   clearEvents()
   storeEvents(events)
   populateEvents(events)
@@ -79,6 +93,7 @@ button.addEventListener('click', async () => {
   }
 })
 
+// Load more search result event
 loadMoreButton.addEventListener('click', async () => {
   const url = `https://app.ticketmaster.com${nextLink}&apikey=${apiKey}`
   const response = await axios.get(url)
@@ -93,10 +108,10 @@ loadMoreButton.addEventListener('click', async () => {
   populateEvents(events)
 })
 
-const loadEvents = () => {
-  let existingEvents = JSON.parse(window.localStorage.getItem('events') || "[]")
-  if (existingEvents) populateEvents(existingEvents)
-}
+// Clear current search results
+clearButton.addEventListener('click', () => {
+  clearEvents()
+})
 
 loadEvents()
 
